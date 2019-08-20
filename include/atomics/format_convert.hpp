f74239e1faab669e81b31cfedc5c310ade217318
SREEJITH1991 <sreejithunnithan1991@gmail.com>
2019-08-19 21:11:35 -0500

    function prototype comments added in the header file

--- include/atomics/format_convert.hpp
+++ include/atomics/format_convert.hpp
@@ -1,12 +1,59 @@
+/** @file format_convert.hpp
+ *  @brief Function prototypes for the file format
+ *
+ *  This contains the prototypes of different functions used
+ *  to extract information from the output log file.
+ *
+ *
+ */
 #ifndef _FORMAT_CONVERT_HPP_
 #define _FORMAT_CONVERT_HPP_
 
+/** @brief get component name from log.
+ *
+ *  This function is invoked to get the component name from the output file
+ *
+ *  @param data The data from which component to be extracted.
+ *  @param component the extracted component
+ *  @return void
+ *
+ */
 void getcomponent(char* data, char* component);
+
+/** @brief get port name from log
+ *
+ *  This function is invoked to get the port name from the output log.
+ *
+ *  @param data The data from which port to be extracted.
+ *  @param port the extracted port
+ *  @return void
+ */
 
 void getport(char* data, char* port);
 
+/** @brief get port value from log
+ *
+ * This function is invoked to get the value of port from the output log
+ *
+ * @param data The data from which value to be extracted
+ * @param value The value of the port
+ * @return true
+ *
+ *
+ */
+
 bool getvalue(char *data, char *value);
 
-void converter(const char *fin, const char *fout);
+/** @brief to format the output file generated
+ *
+ *  This function extracts specific information and writes tabulated result
+ *  in output file
+ *
+ *  @param file_input The output log file generated from sender/receiver/subnet/topmodel
+ *  @param file_output The formatted output file path
+ *
+ */
+
+void converter(const char *file_input, const char *file_output);
 
 #endif // _FORMAT_CONVERT_HPP_
